187073ee0fe95a0e2967a99b691e50a758d78b4d
Mizuho <mizuho128@gmail.com>
2021-09-23 17:38:20 +0900

    lib.rb search function, calling init_projet problem fix

--- setlist.rb
+++ setlist.rb
@@ -20,6 +20,7 @@ parser.on('-m', "--make", "Make setlist") { option[:make] = true }
   parser.on('-r range', "--range", "Select videos from singing streams by Ruby's range expr") {|v|
 		option[:range] = Range.new(*v.split("..").map(&:to_i)) }
   parser.on('-f', "--force", "Overwrite existing setlist info") {|v| option[:force] = true }
+parser.on("--search query", "Search channel by query") {|v| option[:search] =  v}
 
 parser.on('-k KEY', "--api-key", "YouTube API Key file or value") {|v| option[:api_key] = v }
 parser.on('-u URL', "--url", "YouTube channel url") {|v| option[:url] = v }
@@ -31,11 +32,11 @@ rescue StandardError
   exit 1
 end
 
-if option[:url].to_s.empty? then
+if option[:url].to_s.empty? and not option[:search] then
   STDERR.puts("-u URL: Specify YouTube channel's URL and API ")
   exit 2
 end
-if %i[init update make].map{|e| not option.keys.include? e}.all? then
+if %i[init update make search].map{|e| not option.keys.include? e}.all? then
   STDERR.puts("Specify one of them: --init, --update, --make")
   exit 3
 end
@@ -46,7 +47,7 @@ youtube = Google::Apis::YoutubeV3::YouTubeService.new
 youtube.key = option[:api_key]
 
 if option.keys.include? :init then
-  init_project(youtube, option[:url])
+  YTU.init_project(youtube, option[:url])
 elsif option.keys.include?(:make) then
   if option[:song_db].to_s.empty? then
     STDERR.puts("-d songs.csv: Specify CSV file name of list of song names and artists")
@@ -62,4 +63,14 @@ elsif option.keys.include?(:make) then
 elsif option.keys.include?(:update) then
 	require_relative "src/lib"
   YTU.get_uploads(youtube, YTU.url2channel_id(option[:url]))
+
+elsif option.keys.include?(:search) then
+  query = youtube.list_searches("snippet", q: option[:search], type: "channel")
+
+  puts "Found #{query.items.size} results"
+  query.items.each{|item|
+    s = item.snippet
+    puts s.title, "https://www.youtube.com/channel/#{s.channel_id}", ""
+  }
+
 end
